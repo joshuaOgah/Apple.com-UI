8f8562b6e2071c8fe7bcffaa7acce616c741f02f
Joshua Ogah <ogahjoshua7@gmail.com>
2025-12-18 08:51:08 +0100

    responsive version 1.1

--- script.js
+++ script.js
@@ -62,3 +62,62 @@ const swiper = new Swiper("#entertainment-swiper", {
     onlyInViewport: false,
   },
 });
+
+const mobileScreen = matchMedia("(max-width: 412px)").matches;
+
+const changeView = function (screen) {
+  if (screen) {
+    const splider = new Swiper(".splider", {
+      loop: true,
+      lazyloading: true,
+      speed: 1200,
+      spaceBetween: 9,
+      slidesPerView: "auto",
+      centeredSlides: true,
+      keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+
+    const swiper = new Swiper("#entertainment-swiper", {
+      loop: true,
+      lazyloading: true,
+      centeredSlides: true,
+      speed: 1200,
+      spaceBetween: 8,
+      slidesPerView: 3,
+      keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+      },
+    });
+  }
+};
+
+// mobileScreen.addEventListener("change", () => changeView);
+
+///////////////////////////////////////////////////////////
+////////////////////////// FOOTER ////////////////////////
+
+const footerItems = document.getElementsByClassName("footer__nav-item");
+
+const setHeight = function (content) {
+  const height = !content.clientHeight ? content.scrollHeight : 0;
+  content.style.height = `${height}px`;
+};
+
+[...footerItems].forEach((item) => {
+  let dropper = item.children[2];
+  let chevronUp = item.children[1].children[1];
+  let chevronDown = item.children[1].children[0];
+
+  item.addEventListener("click", () => {
+    setHeight(dropper);
+    dropper.classList.toggle("block-display");
+  });
+});
